57e5eaced273e0016bdc1b33ea8f2649b134869e
Vipul Sharma <vipul.sharma20@gmail.com>
2013-12-30 02:17:44 +0530

    update email.py

--- email.py
+++ email.py
@@ -2,7 +2,7 @@ def send_email():
     import smtplib
 
     gmail_user = "sender@gmail.com"
-    gmail_pwd = "giuaqikuqbooaijqq"
+    gmail_pwd = "password-of sender@gmail.com"
     FROM = 'sender@gmail.com'
     TO = ['receiver@gmail.com'] #must be a list
     SUBJECT = "Testing sending using gmail"
