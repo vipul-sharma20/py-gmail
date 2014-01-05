21486e03a62060dd4a466ca43e81206bf018e8c7
Vipul Sharma <vipul.sharma20@gmail.com>
2014-01-06 01:59:37 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,7 +1,7 @@
 def send_email():
-    import smtplib
+    import smtplib                                  # Simple Mail Transfer Protocol module import 
 
-    gmail_user = "sender@gmail.com"
+    gmail_user = "sender@gmail.com"                        
     gmail_pwd = "password-of sender@gmail.com"
     FROM = 'sender@gmail.com'
     TO = ['receiver@gmail.com'] #must be a list
@@ -12,9 +12,9 @@ def send_email():
 	      message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
         """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
         #server = smtplib.SMTP(SERVER) 
-        server = smtplib.SMTP("smtp.gmail.com", 587) #or port 465 doesn't seem to work!
-      	server.ehlo()
-        server.starttls()
+        server = smtplib.SMTP("smtp.gmail.com", 587) # or port 465 doesn't seem to work!
+      	server.ehlo()                                # greeting to receiver smtp, verifies the sender's SMTP
+        server.starttls()                            # Transfer Layer Security, enables encryption
       	server.login(gmail_user, gmail_pwd)
         server.sendmail(FROM, TO, message)
         #server.quit()
