e946440f6c35a097f88aa602ab901ca99c57c822
Vipul <vipul.sharma20@gmail.com>
2017-05-16 12:55:06 +0530

    Refactor: PEP8 compliant
    Hey! Story time:
    * I didn't care about following coding conventions few years ago
    * Then, I realized how you can lose your shit when you come back to
    that same code
    * My life changed, I found enlightenment
    * This commit is to pay for my sins..Oh holy spirit of PEP8.. forgive thy child

--- mail.py
+++ mail.py
@@ -1,25 +1,38 @@
-def send_email():
-    import smtplib                                  # Simple Mail Transfer Protocol module import 
+import smtplib
 
-    gmail_user = "sender@gmail.com"                        
+
+def send_email():
+    gmail_user = "sender@gmail.com"
     gmail_pwd = "password-of sender@gmail.com"
     FROM = 'sender@gmail.com'
-    TO = ['receiver@gmail.com'] #must be a list
+    TO = ['receiver@gmail.com']  # must be a list
     SUBJECT = "Testing sending using gmail"
     TEXT = "Testing sending mail using gmail servers"
-    # Prepare actual message
+
     try:
-	      message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
-        """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
-        #server = smtplib.SMTP(SERVER) 
-        server = smtplib.SMTP("smtp.gmail.com", 587) # or port 465 doesn't seem to work!
-      	server.ehlo()                                # greeting to receiver smtp, verifies the sender's SMTP
-        server.starttls()                            # Transfer Layer Security, enables encryption
-      	server.login(gmail_user, gmail_pwd)
+        # Prepare actual message
+        message = """\From: {from_}\nTo: {to}\nSubject: {subject}\n\n{text}
+        """.format(from_=FROM,
+                   to=", ".join(TO),
+                   subject=SUBJECT,
+                   text=TEXT)
+
+        # or port 465 doesn't seem to work!
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+
+        # greeting to receiver smtp, verifies the sender's SMTP
+        server.ehlo()
+
+        # Transfer Layer Security, enables encryption
+        server.starttls()
+
+        server.login(gmail_user, gmail_pwd)
         server.sendmail(FROM, TO, message)
-        #server.quit()
         server.close()
-        print 'successfully sent the mail'
+        print("successfully sent the mail")
     except:
-        print "failed to send mail"
-send_email()
+        print("failed to send mail")
+
+
+if __name__ == '__main__':
+    send_email()
